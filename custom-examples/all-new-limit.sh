c9dd7ade0e8ca85a2d5ef9dca711f067d5b3735e
Bernhard Mäser <bernhard.maeser@gmail.com>
2012-10-08 17:29:04 +0200

    xt_recent ip_pkt_list_tot bug-hint

--- custom-examples/all-new-limit.sh
+++ custom-examples/all-new-limit.sh
@@ -5,6 +5,10 @@
 ## this is usefull, if someone tries to DOS or synflood your box
 ## and helps to prevent dictonary-attacks
 
+## hint: if you get messages like "xt_recent: hitcount (120) is larger than packets to be remembered (20)"
+## the xt_recent kernel module (called ipt_recent on some systems) is set to remeber only 20 connections
+## see: https://github.com/bmaeser/iptables-boilerplate/issues/1#issuecomment-8935056
+
 
 ## we allow at max 120 new connections per minute
 CONNECTIONS=120
--- custom-examples/ssh-new-limit.sh
+++ custom-examples/ssh-new-limit.sh
@@ -7,6 +7,10 @@
 
 ## this rule does NOT open port 22. it just drops "too many attempts" on port 22
 
+## hint: if you get messages like "xt_recent: hitcount (120) is larger than packets to be remembered (20)"
+## the xt_recent kernel module (called ipt_recent on some systems) is set to remeber only 20 connections
+## see: https://github.com/bmaeser/iptables-boilerplate/issues/1#issuecomment-8935056
+
 
 ## we allow at max 5 new connections per minute
 CONNECTIONS=5
